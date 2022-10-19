74f27ded821add2a5024f561ce8d9a8c95b86b8c
benthecarman <benthecarman@live.com>
2022-10-19 16:45:28 -0500

    Up entropy

--- node-manager/src/seedgen.rs
+++ node-manager/src/seedgen.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use bip39::Mnemonic;
 
 pub fn generate_seed() -> String {
-    let mut entropy = [0u8; 16];
+    let mut entropy = [0u8; 32];
     getrandom::getrandom(&mut entropy).expect("Failed to generate entropy");
     let seed = Mnemonic::from_entropy(&entropy).expect("Could not generate seed");
     seed.to_string()
