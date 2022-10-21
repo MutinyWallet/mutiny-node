fdfc834614c96168d16a11987952e5b5926a4cf7
Paul Miller <paul@paul.lol>
2022-10-21 11:51:09 -0500

    logging and imports

--- node-manager/src/lib.rs
+++ node-manager/src/lib.rs
@@ -2,14 +2,16 @@
 // wasm_bindgen uses improper casing and it needs to be turned off:
 // https://github.com/rustwasm/wasm-bindgen/issues/2882
 
-extern crate cfg_if;
-extern crate wasm_bindgen;
-
 mod nodemanager;
 mod seedgen;
 mod utils;
 
 use cfg_if::cfg_if;
+use futures::{SinkExt, StreamExt};
+use gloo_net::websocket::{futures::WebSocket, Message};
+use log::{debug, info, Level};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
 
 cfg_if! {
     if #[cfg(feature = "wee_alloc")] {
@@ -17,4 +19,32 @@ cfg_if! {
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
+}
+
+#[wasm_bindgen(start)]
+pub async fn main_js() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::new(Level::Debug).message_on_new_line());
+
+    debug!("Main function begins");
+
+    let ws = WebSocket::open("wss://ws.postman-echo.com/raw").unwrap();
+    let (mut write, mut read) = ws.split();
+
+    spawn_local(async move {
+        while let Some(msg) = read.next().await {
+            info!("1. {:?}", msg)
+        }
+        debug!("WebSocket Closed")
+    });
+
+    spawn_local(async move {
+        write
+            .send(Message::Text(String::from("Test from main")))
+            .await
+            .unwrap();
+    });
+
+    debug!("Main function ends");
+
+    Ok(())
 }
--- node-manager/src/nodemanager.rs
+++ node-manager/src/nodemanager.rs
@@ -1,12 +1,18 @@
 use bip39::Mnemonic;
-use std::str::FromStr;
+use futures::{lock::Mutex, stream::SplitSink, SinkExt, StreamExt};
+use gloo_net::websocket::{futures::WebSocket, Message};
+use log::{debug, info};
+use std::{str::FromStr, sync::Arc};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
 
 use crate::{seedgen, utils::set_panic_hook};
 
 #[wasm_bindgen]
 pub struct NodeManager {
     mnemonic: Mnemonic,
+    ws_write: Arc<Mutex<SplitSink<WebSocket, Message>>>,
+    counter: usize,
 }
 
 #[wasm_bindgen]
@@ -21,11 +27,41 @@ impl NodeManager {
             seedgen::generate_seed()
         };
 
-        NodeManager { mnemonic }
+        let ws = WebSocket::open("wss://ws.postman-echo.com/raw").unwrap();
+        let (write, mut read) = ws.split();
+
+        spawn_local(async move {
+            while let Some(msg) = read.next().await {
+                info!("1. {:?}", msg)
+            }
+            debug!("WebSocket Closed")
+        });
+
+        NodeManager {
+            mnemonic,
+            ws_write: Arc::new(Mutex::new(write)),
+            counter: 0,
+        }
     }
 
     #[wasm_bindgen]
     pub fn show_seed(&self) -> String {
         return self.mnemonic.to_string();
+    }
+
+    #[wasm_bindgen]
+    pub fn test_ws(&mut self) {
+        let write = self.ws_write.clone();
+        let count = self.counter;
+        spawn_local(async move {
+            write
+                .clone()
+                .lock()
+                .await
+                .send(Message::Text(format!("Test number {}", count)))
+                .await
+                .unwrap();
+        });
+        self.counter += 1;
     }
 }
