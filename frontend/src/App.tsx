255bbb0eb0e5439dba92b1c0c1f61e35d016e7db
Tony Giorgio <tonygiorgio@protonmail.com>
2022-10-21 11:45:40 -0500

    Add a NodeManager structure that stores seed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import logo from './mutiny-logo.svg';
-import init, { generate_seed, InitOutput } from "node-manager";
+import init, { NodeManager, InitOutput } from "node-manager";
 
 function App() {
 
   const [wasm, setWasm] = useState<InitOutput>();
 
-  const [privatekey, setPrivatekey] = useState("...")
+  const [mnemonic, setMnemonic] = useState("...")
+
+  let myNodeManager = {} as NodeManager
 
   useEffect(() => {
     // TODO: learn why we init this but don't actually call stuff on it
@@ -15,18 +17,25 @@ function App() {
     })
   }, [])
 
+  function createNodeManager() {
+    myNodeManager = new NodeManager(undefined)
+  }
+
   return (
     <div className="p-8 flex flex-col gap-4">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <main>
-        <p>Here's where you put all your money:</p>
+        <p>Here is the seed phrase for your node manager:</p>
         <pre className=''>
-          <code>{privatekey}</code>
+          <code>{mnemonic}</code>
         </pre>
         <p>
-          <button onClick={() => wasm && setPrivatekey(generate_seed())}>Generate!</button>
+          <button onClick={() => wasm && createNodeManager()}>Create the node manager!</button>
+        </p>
+        <p>
+          <button onClick={() => wasm && setMnemonic(myNodeManager.show_seed())}>Reveal Seed!</button>
         </p>
       </main>
     </div>
--- node-manager/src/lib.rs
+++ node-manager/src/lib.rs
@@ -1,10 +1,16 @@
+#![allow(non_snake_case, non_upper_case_globals)]
+// wasm_bindgen uses improper casing and it needs to be turned off:
+// https://github.com/rustwasm/wasm-bindgen/issues/2882
+
 extern crate cfg_if;
 extern crate wasm_bindgen;
 
 mod seedgen;
 mod utils;
 
+use bip39::Mnemonic;
 use cfg_if::cfg_if;
+use std::str::FromStr;
 use wasm_bindgen::prelude::*;
 
 cfg_if! {
@@ -16,6 +22,27 @@ cfg_if! {
 }
 
 #[wasm_bindgen]
-pub fn generate_seed() -> String {
-    seedgen::generate_seed()
+pub struct NodeManager {
+    mnemonic: Mnemonic,
+}
+
+#[wasm_bindgen]
+impl NodeManager {
+    #[wasm_bindgen(constructor)]
+    pub fn new(mnemonic: Option<String>) -> NodeManager {
+        utils::set_panic_hook();
+
+        let mnemonic = if let Some(m) = mnemonic {
+            Mnemonic::from_str(String::as_str(&m)).expect("could not parse specified mnemonic")
+        } else {
+            seedgen::generate_seed()
+        };
+
+        NodeManager { mnemonic }
+    }
+
+    #[wasm_bindgen]
+    pub fn show_seed(&self) -> String {
+        return self.mnemonic.to_string();
+    }
 }
