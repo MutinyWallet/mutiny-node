1719f2b9f6d05c2c876cb8aa9163be090ceef27d
Paul Miller <paul@paul.lol>
2022-10-25 15:28:41 -0500

    try to install clang

--- frontend/build.sh
+++ frontend/build.sh
@@ -9,6 +9,12 @@ source "$HOME/.cargo/env"
 echo "Installing wasm-pack..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
+echo "Installing a recent version of clang"
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add - || exit 1
+sudo apt-add-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-10 main" || exit 1
+sudo apt-get update || exit 1
+sudo apt-get install -y libclang-common-10-dev clang-10 libc6-dev-i386 || exit 1
+
 echo "Building node-manager..."
 npm run build:wasm
 
