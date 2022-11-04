a0bcf5465108755a450faf717db104f408dbb847
Paul Miller <paul@paul.lol>
2022-11-04 14:06:02 -0500

    style index and add fonts

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -3,14 +3,21 @@ import logo from '../images/mutiny-logo.svg';
 
 function App() {
   return (
-    <div className="p-8 flex flex-col gap-4">
-      <header>
+    <div className="flex flex-col h-screen">
+      <header className='p-8'>
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <main className='flex flex-col gap-4'>
-
-        <h1>Welcome to Mutiny</h1>
         <h2>You're probably looking for <a href="/tests">the tests</a></h2>
+      </header>
+      <main className='flex flex-grow flex-col h-full justify-between p-8'>
+
+        <div />
+        <h1 className='text-4xl font-light uppercase'>69_420 <span className='text-2xl'>sats</span></h1>
+        <div />
+        <div className='flex flex-col gap-2 items-start'>
+
+          <button className='green-button'>Send</button>
+          <button className='blue-button'>Deposit</button>
+        </div>
 
       </main>
     </div>
