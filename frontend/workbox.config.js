93b61c78c1410e8156ebf9968822b540cc31461d
Paul Miller <paul@paul.lol>
2022-11-18 18:03:58 -0600

    add craco and craco-workbox to override config

---
+++ frontend/workbox.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    GenerateSW: options => {
+        // override GenerateSW config here
+        // e.g. options.skipWaiting = true;
+        return options;
+    },
+    InjectManifest: options => {
+        // override InjectManifest config here
+        // e.g. options.maximumFileSizeToCacheInBytes = 10 * 1024 * 1024;
+        options.maximumFileSizeToCacheInBytes = 10 * 1024 * 1024;
+        return options;
+    }
+};
