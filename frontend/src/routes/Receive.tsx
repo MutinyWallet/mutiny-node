883172367f4760ff8c87e13d2ee41ad0e9509869
Ben Allen <benalleng@gmail.com>
2022-12-17 12:15:38 -0500

    change input type to number

--- frontend/src/routes/Receive.tsx
+++ frontend/src/routes/Receive.tsx
@@ -33,7 +33,7 @@ function Receive() {
                 <div />
                 <p className="text-2xl font-light">Want some sats?</p>
                 <div className="flex flex-col gap-4">
-                    <input onChange={(e) => setAmount(e.target.value)} className={`w-full ${inputStyle({ accent: "blue" })}`} type="text" placeholder='How much? (optional)' />
+                    <input onChange={(e) => setAmount(e.target.value)} className={`w-full ${inputStyle({ accent: "blue" })}`} type="number" placeholder='How much? (optional)' />
                     <input onChange={(e) => setDescription(e.target.value)} className={`w-full ${inputStyle({ accent: "blue" })}`} type="text" placeholder='What for? (optional)' />
                 </div>
                 <div className='flex justify-start gap-2'>
