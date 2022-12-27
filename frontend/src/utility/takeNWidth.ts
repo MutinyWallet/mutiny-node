81c87355641c670b09a0eb77d5a5bf6bb6020294
Ben Allen <benalleng@gmail.com>
2022-12-27 13:41:16 -0500

    modify takeN function

--- frontend/src/utility/takeNWidth.ts
+++ frontend/src/utility/takeNWidth.ts
@@ -1,4 +1,15 @@
-export default function takeNWidthWidth(s: string, n: number, screenWidth: number): string {
-    let m = (screenWidth * n)
-    return `${s.substring(0, m)}…`
+export default function takeNWidthWidth(s: string, truncStart: number, screenWidth: number): string {
+    console.log(screenWidth)
+    let number = truncStart / s.length
+    let m = (screenWidth / number)
+    let mLong = (screenWidth / (1920 / 120))
+    if (s.length <= 66) {
+        if (s.length > m) {
+            return `${s.substring(0, m)}…`
+        } else {
+            return `${s.substring(0, m)}`
+        }
+    } else {
+        return `${s.substring(0, mLong)}…`
+    }
 }
