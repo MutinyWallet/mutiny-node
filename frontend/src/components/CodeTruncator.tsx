95adec43242f1cc67a4e49cf2d86777449912a39
Ben Allen <benalleng@gmail.com>
2022-12-27 13:38:21 -0500

    add truncator component

---
+++ frontend/src/components/CodeTruncator.tsx
@@ -0,0 +1,16 @@
+import takeNWidth from "@util/takeNWidth"
+import useScreenWidth from "@util/screenWidth"
+
+type Props = {
+    code: string
+    truncStart: number
+  }
+
+export default function CodeTruncator({code, truncStart}: Props) {
+    const screenWidth = useScreenWidth();
+    // the percent is when the width of the screen is 
+
+    return (
+        <span>{takeNWidth(code, truncStart, screenWidth)}</span>
+    )
+}
