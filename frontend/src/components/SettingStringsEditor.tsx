0a25777669278e81d9677d7d07a23d5f0cd393b3
Ben Allen <108441023+benalleng@users.noreply.github.com>
2022-12-30 14:46:11 -0500

    network dropdown selector (#276)
    * network dropdown selector
    
    * rearrange networks
    
    * fix save newtwork prompt
    
    * change dropdown styling
    
    * remove commented input

--- frontend/src/components/SettingStringsEditor.tsx
+++ frontend/src/components/SettingStringsEditor.tsx
@@ -1,26 +1,27 @@
 import { toastAnything } from "@util/dumb";
 import { ChangeEvent, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { inputStyle } from "../styles";
+import { inputStyle, selectStyle } from "../styles";
 import { getExistingSettings, NodeManagerContext, NodeManagerSettingStrings } from "./GlobalStateProvider";
 
 export default function SettingStringsEditor() {
-    let navigate = useNavigate();
     const { setup } = useContext(NodeManagerContext);
 
     const [nodeManagerSettings, setNodeManagerSettings] = useState<NodeManagerSettingStrings>(getExistingSettings());
 
     async function handleSaveSettings() {
+        console.log('save existing network', getExistingSettings().network)
+        console.log('save node manager network', nodeManagerSettings.network)
         try {
             let existingNetwork = getExistingSettings().network;
             if (existingNetwork !== nodeManagerSettings.network) {
                 if (window.confirm("Changing networks will delete your node's state. This can't be undone!")) {
                     localStorage.clear();
+                    await setup(nodeManagerSettings);
+                    window.location.reload();
+                } else {
                     window.location.reload();
                 }
             }
-            await setup(nodeManagerSettings);
-            navigate("/")
         } catch (e) {
             console.error(e)
             toastAnything(e)
@@ -36,12 +37,28 @@ export default function SettingStringsEditor() {
         });
     };
 
+    const handleSelectChange = (name: string) => (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setNodeManagerSettings({
+            ...nodeManagerSettings,
+            [name]: e.target.value,
+        });
+    };
+
     return (
         <>
             <p className="text-2xl font-light">Don't trust us! Use your own servers to back Mutiny</p>
             <div className="flex flex-col gap-2 w-full">
                 <h3 className="text-lg font-light uppercase mt-2">Network</h3>
-                <input onChange={handleInputChange("network")} defaultValue={nodeManagerSettings.network} className={`w-full ${inputStyle({ accent: "blue" })}`} type="text" placeholder='Network' />
+                <div className="flex gap-2">
+                    <div className="select-wrapper bg-red">
+                        <select onChange={handleSelectChange("network")} className={selectStyle({ accent: "blue" })} value={nodeManagerSettings.network} placeholder="Network">
+                            <option className="text-base" value="bitcoin">Mainnet</option>
+                            <option className="text-base" value="testnet">Testnet</option>
+                            <option className="text-base" value="signet">Signet</option>
+                            <option className="text-base" value="regtest">Regtest</option>
+                        </select>
+                    </div>
+                </div>
                 <h3 className="text-lg font-light uppercase mt-2">Esplora</h3>
                 <input onChange={handleInputChange("esplora")} defaultValue={nodeManagerSettings.esplora} className={`w-full ${inputStyle({ accent: "blue" })}`} type="text" placeholder='Esplora' />
                 <h3 className="text-lg font-light uppercase mt-2">Websockets Proxy</h3>
