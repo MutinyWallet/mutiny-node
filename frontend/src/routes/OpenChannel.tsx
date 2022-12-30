41d39c14bb11bd6ef15d63be5addaec4f67783d9
Paul Miller <paul@paul.lol>
2022-12-30 16:42:42 -0600

    add peer select list

--- frontend/src/routes/OpenChannel.tsx
+++ frontend/src/routes/OpenChannel.tsx
@@ -2,17 +2,31 @@ import ActionButton from "@components/ActionButton";
 import AmountInput from "@components/AmountInput";
 import { NodeManagerContext } from "@components/GlobalStateProvider";
 import MutinyToaster from "@components/MutinyToaster";
+import { useQuery } from "@tanstack/react-query";
 import { getFirstNode, toastAnything } from "@util/dumb";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Close from "../components/Close";
 import PageTitle from "../components/PageTitle";
-import { inputStyle, mainWrapperStyle } from "../styles";
+import { inputStyle, mainWrapperStyle, selectStyle } from "../styles";
+import { MutinyPeer } from "node-manager";
+import takeN from "@util/takeN";
 
 
 export default function OpenChannel() {
 	const { nodeManager } = useContext(NodeManagerContext);
 	let navigate = useNavigate();
+
+	const { data: peers } = useQuery({
+		queryKey: ['peers'],
+		queryFn: () => {
+			console.log("Getting peers...")
+			const peers = nodeManager?.list_peers() as Promise<MutinyPeer[]>
+			return peers
+		},
+		enabled: !!nodeManager,
+		refetchInterval: 1000,
+	})
 
 	const [peerPubkey, setPeerPubkey] = useState("");
 	const [channelAmount, setAmount] = useState("")
@@ -46,6 +60,10 @@ export default function OpenChannel() {
 		}
 	}
 
+	function handleSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
+		setPeerPubkey(e.target.value)
+	}
+
 	return (
 		<>
 			<header className='p-8 flex justify-between items-center'>
@@ -58,7 +76,21 @@ export default function OpenChannel() {
 					<div />
 					<p className="text-2xl font-light">Let's do this!</p>
 					<div className="flex flex-col gap-4">
-						<input onChange={(e) => setPeerPubkey(e.target.value)} className={`w-full ${inputStyle({ accent: "blue" })}`} type="text" placeholder='Target node pubkey' />
+						{peers && peers.length < 0 &&
+							<div className="flex flex-col gap-2">
+								<label className="text-xl font-light">Peers</label>
+								<select onChange={handleSelectChange} className={selectStyle({ accent: "blue" })} value={peerPubkey} placeholder="Network">
+									<option>Choose Peer</option>
+									{peers.map(p => <option key={p.pubkey} value={p.pubkey}>{takeN(p.pubkey, 15)}</option>)}
+								</select>
+							</div>
+						}
+						{!peers || peers.length === 0 &&
+							<div className="flex flex-col gap-2">
+								<label>New Peer</label>
+								<input onChange={(e) => setPeerPubkey(e.target.value)} value={peerPubkey} className={`w-full ${inputStyle({ accent: "blue" })}`} type="text" placeholder='Target node pubkey' />
+							</div>
+						}
 						<AmountInput amountSats={channelAmount} setAmount={setAmount} accent="blue" placeholder="How big?" />
 					</div>
 					<ActionButton>Create</ActionButton>
