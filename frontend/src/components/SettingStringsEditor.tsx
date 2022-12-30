e6af0d7bb2845d64f8d2e3c586fee8b036af0e27
Ben Allen <benalleng@gmail.com>
2022-12-30 12:15:28 -0500

    fix save newtwork prompt

--- frontend/src/components/SettingStringsEditor.tsx
+++ frontend/src/components/SettingStringsEditor.tsx
@@ -1,27 +1,27 @@
 import { toastAnything } from "@util/dumb";
 import { ChangeEvent, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { inputStyle } from "../styles";
 import { getExistingSettings, NodeManagerContext, NodeManagerSettingStrings } from "./GlobalStateProvider";
 
 export default function SettingStringsEditor() {
-    let navigate = useNavigate();
     const { setup } = useContext(NodeManagerContext);
 
     const [nodeManagerSettings, setNodeManagerSettings] = useState<NodeManagerSettingStrings>(getExistingSettings());
-    const [selectedNetwork, setSelectedNetwork] = useState(nodeManagerSettings.network);
 
     async function handleSaveSettings() {
+        console.log('save existing network', getExistingSettings().network)
+        console.log('save node manager network', nodeManagerSettings.network)
         try {
             let existingNetwork = getExistingSettings().network;
             if (existingNetwork !== nodeManagerSettings.network) {
                 if (window.confirm("Changing networks will delete your node's state. This can't be undone!")) {
                     localStorage.clear();
+                    await setup(nodeManagerSettings);
+                    window.location.reload();
+                } else {
                     window.location.reload();
                 }
             }
-            await setup(nodeManagerSettings);
-            navigate("/")
         } catch (e) {
             console.error(e)
             toastAnything(e)
@@ -38,7 +38,6 @@ export default function SettingStringsEditor() {
     };
 
     const handleSelectChange = (name: string) => (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedNetwork(e.target.value)
         setNodeManagerSettings({
             ...nodeManagerSettings,
             [name]: e.target.value,
@@ -51,7 +50,7 @@ export default function SettingStringsEditor() {
             <div className="flex flex-col gap-2 w-full">
                 <h3 className="text-lg font-light uppercase mt-2">Network</h3>
                 <form>
-                    <select onChange={handleSelectChange("network")} className={`w-full text-black bg-white text-xl ${inputStyle({ accent: "blue" })}`} name='' value={selectedNetwork} placeholder="Network">
+                    <select onChange={handleSelectChange("network")} className={`w-full text-black bg-white text-xl ${inputStyle({ accent: "blue" })}`} name='' value={nodeManagerSettings.network} placeholder="Network">
                         <option className="text-base" value="bitcoin">Mainnet</option>
                         <option className="text-base" value="testnet">Testnet</option>
                         <option className="text-base" value="signet">Signet</option>
