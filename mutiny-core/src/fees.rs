30f9260f0772366d17288ff4715e26deead08346
benthecarman <benthecarman@live.com>
2023-04-26 18:37:04 -0500

    Add test for fee rate calc

--- mutiny-core/src/fees.rs
+++ mutiny-core/src/fees.rs
@@ -56,3 +56,72 @@ fn fallback_fee_from_conf_target(confirmation_target: ConfirmationTarget) -> u32
         ConfirmationTarget::HighPriority => 5000,
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::indexed_db::MutinyStorage;
+    use crate::test_utils::*;
+    use std::collections::HashMap;
+
+    use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[test]
+    fn test_num_blocks_from_conf_target() {
+        assert_eq!(
+            num_blocks_from_conf_target(ConfirmationTarget::Background),
+            12
+        );
+        assert_eq!(num_blocks_from_conf_target(ConfirmationTarget::Normal), 6);
+        assert_eq!(
+            num_blocks_from_conf_target(ConfirmationTarget::HighPriority),
+            3
+        );
+    }
+
+    #[test]
+    fn test_fallback_fee_from_conf_target() {
+        assert_eq!(
+            fallback_fee_from_conf_target(ConfirmationTarget::Background),
+            253
+        );
+        assert_eq!(
+            fallback_fee_from_conf_target(ConfirmationTarget::Normal),
+            2000
+        );
+        assert_eq!(
+            fallback_fee_from_conf_target(ConfirmationTarget::HighPriority),
+            5000
+        );
+    }
+
+    #[test]
+    async fn test_get_est_sat_per_1000_weight() {
+        let storage = MutinyStorage::new("".to_string()).await.unwrap();
+        let mut fee_estimates = HashMap::new();
+        fee_estimates.insert("6".to_string(), 10_f64);
+        storage.insert_fee_estimates(fee_estimates).unwrap();
+
+        let fee_estimator = MutinyFeeEstimator::new(storage);
+
+        // test that we get the fee rate from the cache
+        assert_eq!(
+            fee_estimator.get_est_sat_per_1000_weight(ConfirmationTarget::Normal),
+            2500
+        );
+
+        // test that we get the fallback fee rate
+        assert_eq!(
+            fee_estimator.get_est_sat_per_1000_weight(ConfirmationTarget::Background),
+            253
+        );
+        assert_eq!(
+            fee_estimator.get_est_sat_per_1000_weight(ConfirmationTarget::HighPriority),
+            5000
+        );
+
+        cleanup_wallet_test().await;
+    }
+}
