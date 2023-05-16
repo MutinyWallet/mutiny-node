3308d3377032c7fe85cf2abb9ae96c525619961b
Tony Giorgio <tonygiorgio@protonmail.com>
2023-05-15 19:00:59 -0500

    Update existing channels to match default config

--- mutiny-core/src/node.rs
+++ mutiny-core/src/node.rs
@@ -245,6 +245,36 @@ impl Node {
         let channel_manager: Arc<PhantomChannelManager> =
             Arc::new(read_channel_manager.channel_manager);
 
+        // Check all existing channels against default configs.
+        // If we have default config changes, those should apply
+        // to all existing and new channels.
+        let default_config = default_user_config().channel_config;
+        for channel in channel_manager.list_channels() {
+            // unwrap is safe after LDK.0.0.109
+            if channel.config.unwrap() != default_config {
+                match channel_manager.update_channel_config(
+                    &channel.counterparty.node_id,
+                    &[channel.channel_id],
+                    &default_config,
+                ) {
+                    Ok(_) => {
+                        log_debug!(
+                            logger,
+                            "changed default config for channel: {}",
+                            channel.channel_id.to_hex()
+                        )
+                    }
+                    Err(e) => {
+                        log_error!(
+                            logger,
+                            "error changing default config for channel: {} - {e:?}",
+                            channel.channel_id.to_hex()
+                        )
+                    }
+                };
+            }
+        }
+
         let route_handler = Arc::new(GossipMessageHandler {
             network_graph: gossip_sync.network_graph().clone(),
             logger: logger.clone(),
