77d13a27a13dd7668ba758de0694e016c47bb67e
benthecarman <15256660+benthecarman@users.noreply.github.com>
2023-05-24 18:11:49 -0500

    De-wasm sleep (#532)

--- mutiny-core/src/utils.rs
+++ mutiny-core/src/utils.rs
@@ -8,14 +8,21 @@ use lightning::util::ser::Writeable;
 use lightning::util::ser::Writer;
 
 pub async fn sleep(millis: i32) {
-    let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
-        web_sys::window()
-            .unwrap()
-            .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, millis)
-            .unwrap();
-    };
-    let p = js_sys::Promise::new(&mut cb);
-    wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
+    #[cfg(target_arch = "wasm32")]
+    {
+        let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
+            web_sys::window()
+                .unwrap()
+                .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, millis)
+                .unwrap();
+        };
+        let p = js_sys::Promise::new(&mut cb);
+        wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        std::thread::sleep(Duration::from_millis(millis));
+    }
 }
 
 pub fn now() -> Duration {
