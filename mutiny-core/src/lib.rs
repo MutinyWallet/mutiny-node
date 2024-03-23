3845aeef012718085b608abf355675fcb5ce1e69
benthecarman <benthecarman@live.com>
2024-03-23 15:55:58 -0500

    Update fedimint to v0.3.0rc2

--- mutiny-core/src/lib.rs
+++ mutiny-core/src/lib.rs
@@ -865,9 +865,13 @@ impl<S: MutinyStorage> MutinyWalletBuilder<S> {
         let federations = if !federation_storage.federations.is_empty() {
             let start = Instant::now();
             log_trace!(logger, "Building Federations");
-            let result =
-                create_federations(federation_storage.clone(), &config, &self.storage, &logger)
-                    .await?;
+            let result = create_federations(
+                federation_storage.clone(),
+                &config,
+                self.storage.clone(),
+                &logger,
+            )
+            .await?;
             log_debug!(
                 logger,
                 "Federations started, took: {}ms",
@@ -2358,23 +2362,6 @@ impl<S: MutinyStorage> MutinyWallet<S> {
         Ok(())
     }
 
-    pub async fn recover_federation_backups(&self) -> Result<(), MutinyError> {
-        let federation_ids = self.list_federation_ids().await?;
-
-        let federations = self.federations.read().await;
-        for fed_id in federation_ids {
-            federations
-                .get(&fed_id)
-                .ok_or(MutinyError::NotFound)?
-                .recover_backup()
-                .await?;
-
-            log_info!(self.logger, "Scanned federation backups: {}", fed_id);
-        }
-
-        Ok(())
-    }
-
     pub async fn get_total_federation_balance(&self) -> Result<u64, MutinyError> {
         let federation_ids = self.list_federation_ids().await?;
         let mut total_balance = 0;
@@ -2851,7 +2838,7 @@ impl<S: MutinyStorage> InvoiceHandler for MutinyWallet<S> {
 async fn create_federations<S: MutinyStorage>(
     federation_storage: FederationStorage,
     c: &MutinyWalletConfig,
-    storage: &S,
+    storage: S,
     logger: &Arc<MutinyLogger>,
 ) -> Result<Arc<RwLock<HashMap<FederationId, Arc<FederationClient<S>>>>>, MutinyError> {
     let mut federation_map = HashMap::with_capacity(federation_storage.federations.len());
@@ -2860,7 +2847,7 @@ async fn create_federations<S: MutinyStorage>(
             uuid,
             federation_index.federation_code,
             c.xprivkey,
-            storage,
+            storage.clone(),
             c.network,
             logger.clone(),
         )
@@ -2910,7 +2897,7 @@ pub(crate) async fn create_new_federation<S: MutinyStorage>(
         next_federation_uuid.clone(),
         federation_code.clone(),
         xprivkey,
-        &storage,
+        storage.clone(),
         network,
         logger.clone(),
     )
