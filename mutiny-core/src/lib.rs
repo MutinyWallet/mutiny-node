96ee7c972b1a1a3f094aa3a38fb802f30377c705
elnosh <elnosh@pm.me>
2024-03-25 17:28:27 -0500

    extractor to handle error responses

--- mutiny-core/src/lib.rs
+++ mutiny-core/src/lib.rs
@@ -714,36 +714,6 @@ pub struct MutinyWalletConfig {
     skip_hodl_invoices: bool,
 }
 
-impl MutinyWalletConfig {
-    pub fn new(
-        xprivkey: ExtendedPrivKey,
-        network: Network,
-        esplora: Option<String>,
-        rgs: Option<String>,
-        lsp: Option<String>,
-    ) -> MutinyWalletConfig {
-        MutinyWalletConfig {
-            xprivkey,
-            #[cfg(target_arch = "wasm32")]
-            websocket_proxy_addr: None,
-            network,
-            user_esplora_url: esplora,
-            user_rgs_url: rgs,
-            lsp_url: lsp,
-            lsp_token: None,
-            lsp_connection_string: None,
-            auth_client: None,
-            subscription_url: None,
-            scorer_url: None,
-            primal_url: None,
-            do_not_connect_peers: false,
-            skip_device_lock: false,
-            safe_mode: false,
-            skip_hodl_invoices: true,
-        }
-    }
-}
-
 pub struct MutinyWalletBuilder<S: MutinyStorage> {
     xprivkey: ExtendedPrivKey,
     nostr_key_source: NostrKeySource,
